823e5a359558c9494d2db5b5152efb8eb4295595
Kristof <danyi.kristof1998@gmail.com>
2024-11-20 17:30:43 +0100

    compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.4.0"
-    kotlin("android") version "1.9.0"
+    kotlin("android") version "1.9.10"
     kotlin("kapt")
 }
 
@@ -20,6 +20,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 
     compileOptions {
@@ -33,24 +34,29 @@ android {
 
     android {
         composeOptions {
-            kotlinCompilerExtensionVersion = "1.5.1"
+            kotlinCompilerExtensionVersion = "1.5.3"
         }
     }
+
 
 }
 
 dependencies {
+    // Core AndroidX and Material dependencies
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Room Database
     implementation("androidx.room:room-runtime:2.5.1")
-    implementation(libs.androidx.junit.ktx)
-    testImplementation("junit:junit:4.12")
     kapt("androidx.room:room-compiler:2.5.1")
+
+    // Lifecycle components
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    // Compose dependencies
     implementation("androidx.compose.ui:ui:1.5.3")
     implementation("androidx.compose.material:material:1.5.3")
     implementation("androidx.compose.material3:material3:1.1.1")
@@ -58,5 +64,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
+
+    // Test dependencies
+    implementation("androidx.test.ext:junit-ktx:1.2.1")
+    testImplementation("junit:junit:4.13.2")
 }
 
